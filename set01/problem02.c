ab6ff08419f5d03095bf8b15bc5f917268bec99d
Priyaanshu  Arora <113001865+Priyaanshu10@users.noreply.github.com>
2022-09-14 05:45:35 +0000

    Now i can add 2 scaned numbers

---
+++ set01/problem02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    printf("Enter the value of b");
+    scanf("%d",&b);
+    printf("%d + %d = %d",a,b,a+b);
+    return 0;
+}
