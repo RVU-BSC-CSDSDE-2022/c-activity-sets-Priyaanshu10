1d62754b34ca5934a042c5f596796bec53cda8bb
Priyaanshu  Arora <113001865+Priyaanshu10@users.noreply.github.com>
2022-09-14 06:44:25 +0000

    in my 3rd program i added 3 number through pass by value

--- set01/problem02.c
+++ set01/problem02.c
@@ -2,9 +2,9 @@
 
 int main(void){
     int a,b;
-    printf("Enter the value of a");
+    printf("Enter the value of a\n");
     scanf("%d",&a);
-    printf("Enter the value of b");
+    printf("Enter the value of b\n");
     scanf("%d",&b);
     printf("%d + %d = %d",a,b,a+b);
     return 0;
---
+++ set01/problem03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("Enter any number\n");
+  scanf("%d",&a);
+  return a;
+}
+int find_sum(int a, int b, int d)
+{
+  int sum;
+  sum=a+b+d;
+  return sum;
+}
+void output(int a, int b, int d, int c)
+{
+  printf("Sum of %d and %d and %d is %d",a,b,d,c);
+}
+int main()
+{
+  int x,y,q,z;
+  x=input();
+  y=input();
+  q=input();
+  z= find_sum(x,y,q);
+  output(x,y,q,z);
+  return 0;
+}
