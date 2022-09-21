5e4a7e001c652389488c1cacc8c7cb4812bfaaa5
Priyaanshu  Arora <113001865+Priyaanshu10@users.noreply.github.com>
2022-09-21 07:09:16 +0000

    7

---
+++ set01/problem07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int n, i, sum;
+
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  sum=0;
+  for (i = 1; i <= n;i++)
+{
+  sum=sum+i;
+}
+  printf("Sum=%d\n",sum);
+  return 0;
+}
