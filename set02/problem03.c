668b06a81a7f1f7d3e978e054b0575c7ab5b2fe2
Priyaanshu  Arora <113001865+Priyaanshu10@users.noreply.github.com>
2022-11-09 11:09:31 +0000

    2.4

---
+++ set02/problem03.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+struct camel
+{
+	float radius, height, length,weight;
+};
+typedef struct camel Camel;
+
+Camel input()
+{ Camel c;
+  printf("Enter the radius\n");
+  scanf("%f",&c.radius);
+
+  printf("Enter the height\n");
+  scanf("%f",&c.height);
+
+  printf("Enter the lenght\n");
+  scanf("%f",&c.length);
+  return c;
+}
+
+void find_weight(Camel *c) 
+{
+  int a,b,d;
+  a= pow(c->radius,3);
+  b= c->height*c->length;
+  d= sqrt(b);
+  c->weight= 3.14*a*d;
+}
+
+void output(Camel c)
+{ 
+  printf("The weight of camel is %f\n",c.weight);
+}
+
+int main()
+  {
+  Camel z;
+  z= input();
+  find_weight(&z);
+  output(z);
+  }
---
+++ set02/problem05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math>
+
+int input()
+{
+  int x;
+  printf("Enter x");
+  scanf("%f",&x);
+  return x;
+}
+
+float borga_X(int x)
+{
+  
+}
