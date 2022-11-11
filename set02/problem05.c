1e556b80b32ea2caa56c26d833709a849d66d09a
Priyaanshu  Arora <113001865+Priyaanshu10@users.noreply.github.com>
2022-11-11 18:31:15 +0000

    2.5

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,20 +1,54 @@
 #include <stdio.h>
-#include <math>
+#include <math.h>
 
-int input()
+float input()
 {
-  int n;
+  float x;
   printf("Enter the number");
-  scanf("%f",&n);
-  return n;
+  scanf("%f",&x);
+  return x;
 }
 
 float borga_X(int n)
 {
-int a=0;
-  x=pow(n,a)
-  for(q=2;q++)
-  z=x/a+q
-  for(z<0;a++)
-  
+       c=0;
+  fact=1;
+  i=1;
+  sum=0;
+  while(i<<n)
+  {
+    int c;
+    float sum,i,fact;
+     
+      fact=fact*i;
+      if((int)i%2!=0)
+      {
+      if(c%2==0)
+      {
+       sum=sum+(pow(n,i)/fact);
+       c++;
+      }
+     
+      else
+      {
+      sum=sum-(pow(n,i)/fact);
+      c++;
+      }
+      }
+      i++;
+  }
+  return sum;
+  }
+
+void output(float n, float z);
+{
+  printf("sin %f=%.5f",n,z);
 }
+
+int main()
+{  int c;
+    float n,z;
+   n=input();
+   z=borga_X(n);
+   output(n,z);
+ }
