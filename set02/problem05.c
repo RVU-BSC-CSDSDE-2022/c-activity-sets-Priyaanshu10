7239888a91a0f0c29a997d211015f238c787164a
Priyaanshu  Arora <113001865+Priyaanshu10@users.noreply.github.com>
2022-11-16 07:18:24 +0000

    Finally done problem number 5 of set 2 with functions.....

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
+int
+input ()
 {
-    float sum,term,p;
-    int x,j,i,fact;
-    printf("Enter x ");
-    scanf("%d",&x);
-    sum =0;
-    fact = 1, term = 1; 
-    for (i=1;term>0.000001;i++)
+  int x;
+  printf ("Enter x ");
+  scanf ("%d", &x);
+  return x;
+}
+
+float
+borga_X (int x)
+{
+  float sum, term, p;
+  int j, i, fact;
+  sum = 0;
+  fact = 1, term = 1;
+  for (i = 1; term > 0.000001; i++)
     {
-        fact=fact*i;
-        if(i%2!=0)
-        {
-            j=(i-1)/2;
-            p=pow(x,j);
-            term=p/fact;
-            sum=sum+term;
-        }
+      fact = fact * i;
+      if (i % 2 != 0)
+	{
+	  j = (i - 1) / 2;
+	  p = pow (x, j);
+	  term = p / fact;
+	  sum = sum + term;
+	}
     }
-    printf("%f",sum);
-    return 0;
+  return sum;
+}
+
+void
+output (int x, float sum)
+{
+  printf ("borga(%d) = %f", x, sum);
+}
+
+int
+main ()
+{
+  float y;
+  int x;
+  x = input ();
+  y = borga_X (x);
+  output (x, y);
+  return 0;
 }
